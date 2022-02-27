7b4103b00de90e7cad066a7cf951c6c730b43f0d
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-02-27 14:03:15 +0530

    final commit

--- core/models.py
+++ core/models.py
@@ -1,37 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
-# Create your models here.
-
 
 AGE_CHOICES=(
-    ('All', 'All'),
-    ('Kids', 'Kids')
+    ('All','All'),
+    ('Kids','Kids')
 )
 
-MOVIE_CHOICES=(
-    ('seasonal', 'Seasonal'),
-    ('single', 'Single')
+MOVIE_TYPE=(
+    ('single','Single'),
+    ('seasonal','Seasonal')
 )
 
 class CustomUser(AbstractUser):
-    profiles = models.ManyToManyField('Profile', blank=True)
+    profiles=models.ManyToManyField('Profile')
+
 
 class Profile(models.Model):
     name=models.CharField(max_length=225)
-    age_limit=models.CharField(max_length=10, choices=AGE_CHOICES)
-    uuid=models.UUIDField(default=uuid.uuid4)
+    age_limit=models.CharField(max_length=5,choices=AGE_CHOICES)
+    uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+
+
+    def __str__(self):
+        return self.name +" "+self.age_limit
 
 class Movie(models.Model):
-    title=models.CharField(max_length=225)
-    discription=models.TextField(blank=True, null=True)
-    created=models.DateTimeField(auto_now_add=True)
-    uuid=models.UUIDField(default=uuid.uuid4)
-    types=models.CharField(max_length=10, choices=MOVIE_CHOICES)
-    videos=models.ManyToManyField('video')
-    flyer=models.ImageField(upload_to='flyers')
-    age_limit=models.CharField(max_length=10, choices=AGE_CHOICES)
+    title:str=models.CharField(max_length=225)
+    description:str=models.TextField()
+    created =models.DateTimeField(auto_now_add=True)
+    uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+    type=models.CharField(max_length=10,choices=MOVIE_TYPE)
+    videos=models.ManyToManyField('Video')
+    flyer=models.ImageField(upload_to='flyers',blank=True,null=True)
+    age_limit=models.CharField(max_length=5,choices=AGE_CHOICES,blank=True,null=True)
 
 class Video(models.Model):
-    title=models.CharField(max_length=225, blank=True, null=True)
+    title:str = models.CharField(max_length=225,blank=True,null=True)
     file=models.FileField(upload_to='movies')
+    
+    
+
+    
+
